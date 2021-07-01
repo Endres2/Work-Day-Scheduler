7a82a47d2dec0e199981e7dabbb3367760c43236
Endres <andresdhr99@gmail.com>
2021-07-01 17:09:56 -0400

    Created functionality to display different styling based on current hour

--- script.js
+++ script.js
@@ -9,10 +9,11 @@ dateOnScreen.append(currentTime);
 var saveBtn = $(".saveBtn");
 
 
-
+//function call that executes when save button is pressed
 saveBtn.on("click",function(){
-    
+    //gets text value of text area
     var textValue = $(this).siblings(".description").val();
+    //gets id of time id div
     var time = $(this).parent().attr("id");
     localStorage.setItem(time, textValue);
     //console.log(localStorage.getItem("hour-11"));
@@ -27,23 +28,30 @@ saveBtn.on("click",function(){
 });
 
 $(document).ready(function(){
+    //created variable to get current time as hour
     var hour = moment().format("HH");
-    var hourId = "hour-"+hour;
+    //This function runs through each .description element
     $(".description").each(function(i,element){
+        //saves the id of the parent node or the time id
         var currentKey = element.parentNode.getAttribute("id");
+        //gets  the id saved in local storage at this item
         $(this).val(localStorage.getItem(currentKey));
-        // if(hour === currentKey){
-
-        // }
+        //slices time id to get the las two digits to compare with current hour
+        var hourId = currentKey.slice(5,7);
+        //if current hour is the same  as the id add present ccs style
+        if(hour === hourId){
+            $(this).addClass("present");
+        }
+        //if current hour is the less  as the id add future ccs style
+        else if(hour < hourId){
+            $(this).addClass("future");
+        }
+        //if current hour is the greater  as the id add past ccs style
+        else{
+            $("textarea").addClass("past");
+        }
     });
     
 });
-
-
-
-
-
-
-
 
 });
